02965feddf5a06f78cdd42a43748400a160357d6
Richard Gordon <richgordon95@gmail.com>
2020-09-27 23:55:07 -0400

    async await refactor

--- src/prisma.js
+++ src/prisma.js
@@ -6,41 +6,43 @@ const prisma = new Prisma({
 });
 
 
-// prisma.query.users(null, '{ id name email posts { id title} }').then((data) => {
-// console.log(JSON.stringify(data, undefined, 2));
-// });
+const createPostForUser = async (authorId, data) => {
+    const post = await prisma.mutation.createPost({
+        data: {
+            ...data,
+            author: {
+                connect: {
+                   id: authorId
+               }
+           }
+        }
+    }, '{ id }')
+    const user = await prisma.query.user({
+        where: {
+            id: authorId
+        }
+    }, '{ id name email posts { id title published body } } ')
+        return user
+    }
 
-// prisma.query.comments(null, '{ id body author { name } }').then((data) => {
-//     console.log(JSON.stringify(data, undefined, 2))
-// });
+    // createPostForUser("ckflwn3no00rf0833su720yxb", {
+    //     title: 'newest post',
+    //     body: "the war of art",
+    //     published: true
+    // }).then((user) => {
+    //     console.log(JSON.stringify(user, undefined, 2))
+    // })
 
-// prisma.mutation.createPost({
+
+// prisma.mutation.updatePost({
 //     data: {
-//         title: "my old graphql post",
-//         body: "h",
-//         published: false,
-//         author: {
-//             connect: {
-//                 id: "ckflwn3no00rf0833su720yxb"
-//             }
-//         }
-//     }
-// }, '{ id title body published }').then((data) => {
-//     console.log(JSON.stringify(data, undefined, 2))
-//     return prisma.query.users(null, '{ id name posts { id title } } ').then((data) => {
+//         title: "changed title",
+//         body: "H",
+//         published: true,
+//     }, where: { id: "ckflzcbbn02fq0833rifd3zi0"}
+// }, '{ id title body published } ').then((data) => {
+//     return prisma.query.posts(null, ' { id title body published author { name } } ').then((data) => {
 //         console.log(JSON.stringify(data, undefined, 2))
+
 //     })
-// });
-
-prisma.mutation.updatePost({
-    data: {
-        title: "changed title",
-        body: "H",
-        published: true,
-    }, where: { id: "ckflzcbbn02fq0833rifd3zi0"}
-}, '{ id title body published } ').then((data) => {
-    return prisma.query.posts(null, ' { id title body published author { name } } ').then((data) => {
-        console.log(JSON.stringify(data, undefined, 2))
-
-    })
-})
+// })

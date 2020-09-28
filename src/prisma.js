34d92bc687d55bbe6cbc927daf5f0482bea02782
Richard Gordon <richgordon95@gmail.com>
2020-09-27 23:34:14 -0400

    prisma mutation  ex 1

--- src/prisma.js
+++ src/prisma.js
@@ -10,6 +10,21 @@ prisma.query.users(null, '{ id name email posts { id title} }').then((data) => {
 console.log(JSON.stringify(data, undefined, 2));
 });
 
-prisma.query.comments(null, '{ id body author { name } }').then((data) => {
+// prisma.query.comments(null, '{ id body author { name } }').then((data) => {
+//     console.log(JSON.stringify(data, undefined, 2))
+// });
+
+prisma.mutation.createPost({
+    data: {
+        title: "my new graphql post",
+        body: "here is my body text",
+        published: true,
+        author: {
+            connect: {
+                id: "ckflwn3no00rf0833su720yxb"
+            }
+        }
+    }
+}, '{ id title body published }').then((data) => {
     console.log(JSON.stringify(data, undefined, 2))
 });

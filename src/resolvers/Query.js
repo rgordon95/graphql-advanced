53202ca2765d3fc91f9fc2d3e4f751cb67268ccf
Richard Gordon <richgordon95@gmail.com>
2020-09-28 21:38:19 -0400

    query real db part 2

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,4 +1,4 @@
-import { Prisma } from "prisma-binding";
+import prisma from '../prisma';
 
 const Query = {
     me() {
@@ -18,16 +18,18 @@ const Query = {
             author: '1',
        }
     },
-    posts(parent, args, { db }, info) {
-       if (!args.query) {
-           return db.posts;
-       }
+    posts(parent, args, { db, prisma }, info) {
+        return prisma.query.posts(null, info);
+
+        //        if (!args.query) {
+//            return db.posts;
+//        }
        
-       return db.posts.filter((post) => {
-            const isTitleMatch = post.title.toLowerCase().includes(args.query.toLowerCase());
-            const isBodyMatch = post.body.toLowerCase().includes(args.query.toLowerCase());
-           return isTitleMatch || isBodyMatch;  
-   })
+//        return db.posts.filter((post) => {
+//             const isTitleMatch = post.title.toLowerCase().includes(args.query.toLowerCase());
+//             const isBodyMatch = post.body.toLowerCase().includes(args.query.toLowerCase());
+//            return isTitleMatch || isBodyMatch;  
+//    })
 },
 users(parent, args, { db, }, info) {
 return prisma.query.users(null, info)
